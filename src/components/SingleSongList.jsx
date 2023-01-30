6ecfad1ba08e6fbee99e77c142920b1db1012349
Dipu <dipuchaurasiya91@gmail.com>
2023-01-30 22:55:47 +0545

    image added for song list

--- src/components/SingleSongList.jsx
+++ src/components/SingleSongList.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import { usePlayerContext } from "../Context/PlayerContext";
-const SingleSongList = ({ id, name, primaryArtists, duration, index }) => {
+import { ImageFetch } from "../Utils/Helper";
+const SingleSongList = ({
+  id,
+  name,
+  primaryArtists,
+  duration,
+  index,
+  image,
+}) => {
   const { singleSong } = usePlayerContext();
   let currentIndex = <p className="text-slate-300">{index}</p>;
   let playButton = <PlayArrowIcon className="text-darkTitle text-xl" />;
@@ -10,12 +18,17 @@ const SingleSongList = ({ id, name, primaryArtists, duration, index }) => {
   return (
     <div
       data-id={id}
-      className="grid grid-cols-[max-content,auto,max-content] gap-3 cursor-pointer bg-lightBlue rounded-lg shadow-xl py-4 items-center px-5"
+      className="grid grid-cols-[max-content,max-content,auto,max-content] gap-3 cursor-pointer bg-lightBlue rounded-lg shadow-xl py-4 items-center px-5"
       onMouseEnter={() => setPlayBtn(playButton)}
       onMouseLeave={() => setPlayBtn(currentIndex)}
       onClick={() => singleSong(id)}
     >
       <div className="w-4">{PlayBtn}</div>
+      <img
+        src={image[1].link}
+        className="w-14 rounded-lg object-cover"
+        alt={name}
+      />
       <div>
         <h3 className="text-slate-200">{name}</h3>
         <p className="text-xs opacity-90 mt-[2px]   text-darkTextColor tracking-wide">

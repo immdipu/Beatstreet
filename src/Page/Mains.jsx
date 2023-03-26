24e2c26f5d9561eba89d4e609f2fac18e32fd7a0
Dipu <dipuchaurasiya91@gmail.com>
2023-03-26 13:42:08 +0545

    bug fixed

--- src/Page/Mains.jsx
+++ src/Page/Mains.jsx
@@ -14,11 +14,6 @@ import {
 const Mains = () => {
   const { homeData_loading: loading } = useMusicContext();
   const { side_menu_show } = usePlayerContext();
-
-  useEffect(() => {
-    AutoLogin();
-  }, []);
-
   if (loading) {
     return (
       <div className="text-2xl font-bold fixed inset-0 w-full h-full flex place-items-center justify-center bg-darkBlue -z-20 pr-32 max-md:pr-0 ">

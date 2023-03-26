f41ee377e13c8b4f3f0d961b5d22d82478e3c7dc
Dipu <dipuchaurasiya91@gmail.com>
2023-03-26 13:43:51 +0545

    bug fixed

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -8,7 +8,7 @@ const TopNav = () => {
   const { login_success, AutoLogin } = useUserContext();
   useEffect(() => {
     AutoLogin();
-  });
+  }, []);
   return (
     <div className=" border-b-2 border-slate-100 border-opacity-10 h-20">
       <div className="h-20 flex items-center px-9 justify-between fixed z-40 backdrop-blur-sm bg-darkBlue bg-opacity-60 right-0 left-0 ml-52 max-md:ml-0 overflow-hidden top-0">

42302ccdf380e5c8ffe536c8a1eeb8d03992bb6a
Dipu <dipuchaurasiya91@gmail.com>
2023-04-01 09:52:38 +0545

    pwa updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+
+      manifest: {
+        name: "BeatStreet",
+        short_name: "B",
+        description:
+          "Beatstreet is the ultimate destination for streaming and downloading music for free.",
+        icons: [
+          {
+            src: "/images/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/images/maskable_icon.png",
+            sizes: "203x203",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#16191e",
+        theme_color: "#1d242c",
+      },
+    }),
+  ],
+});

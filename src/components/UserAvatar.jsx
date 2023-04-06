ee034a58b827cedcf79f76d284ccb9a0e385cb93
Dipu <dipuchaurasiya91@gmail.com>
2023-04-07 00:06:07 +0545

    image for admin added

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -4,10 +4,14 @@ import UserDropDown from "./UserDropDown";
 import { AnimatePresence, motion } from "framer-motion";
 
 const UserAvatar = () => {
-  const { user_name, user_drop_down, HandleUserDropDown } = useUserContext();
+  const { user_name, User_id, user_drop_down, HandleUserDropDown } =
+    useUserContext();
   const imageRef = useRef();
 
   let Imagelink = `https://api.dicebear.com/5.x/initials/svg?seed=${user_name}`;
+  if (User_id === process.env.ID) {
+    Imagelink = "https://avatars.githubusercontent.com/u/103568666?v=4";
+  }
   return (
     <div
       className="w-12 rounded-full ml-2 select-none cursor-pointer relative h-12 "

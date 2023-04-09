ed3c4d887aa395943689ee5c6eb27987af3ef264
Dipu <dipuchaurasiya91@gmail.com>
2023-04-09 11:25:45 +0545

    font size fixed

--- src/Page/UserSinglePlaylist.jsx
+++ src/Page/UserSinglePlaylist.jsx
@@ -42,8 +42,8 @@ const UserSinglePlaylist = () => {
       </section>
       <section className=" px-14 max-md:px-2 overflow-auto pb-8">
         <h3 className="text-neutral-50  text-2xl flex items-center max-md:text-xl px-4 mb-5">
-          {user_single_playlist.name} -
-          <span className="text-base">
+          {user_single_playlist.name} -{" "}
+          <span className="text-sm text-neutral-300">
             {user_single_playlist.songs.length} songs
           </span>
         </h3>

9db97506fd3888eb369cc35a231c9741370074ba
Dipu <dipuchaurasiya91@gmail.com>
2023-04-09 11:29:11 +0545

    Download all added for playlist

--- src/Page/UserSinglePlaylist.jsx
+++ src/Page/UserSinglePlaylist.jsx
@@ -4,6 +4,8 @@ import { usePlayerContext } from "../Context/PlayerContext";
 import { useUserContext } from "../Context/UserContext";
 import { LoadingSpinner, SongsList } from "../components";
 import { Logo, LogoText } from "../components";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
+import RippleButton from "ripple-effect-reactjs";
 
 const UserSinglePlaylist = () => {
   let { id } = useParams();
@@ -30,6 +32,13 @@ const UserSinglePlaylist = () => {
     );
   }
 
+  const HandleDownloadAll = () => {
+    const btns = document.querySelectorAll(".btnss");
+    btns.forEach((btn) => {
+      btn.click();
+    });
+  };
+
   return (
     <div>
       <section className=" flex justify-center items-center py-20  rounded-b-2xl mb-8 relative h-40">
@@ -41,12 +50,27 @@ const UserSinglePlaylist = () => {
         </div>
       </section>
       <section className=" px-14 max-md:px-2 overflow-auto pb-8">
-        <h3 className="text-neutral-50  text-2xl flex items-center max-md:text-xl px-4 mb-5">
-          {user_single_playlist.name} -{" "}
-          <span className="text-sm text-neutral-300">
-            {user_single_playlist.songs.length} songs
-          </span>
-        </h3>
+        <section className="flex justify-between items-center">
+          <h3 className="text-neutral-50  text-2xl flex items-center max-md:text-xl px-4 mb-5">
+            {user_single_playlist.name} -{" "}
+            <span className="text-sm text-neutral-300">
+              {user_single_playlist.songs.length} songs
+            </span>
+          </h3>
+          <div
+            className="w-[38px] max-md:mt-4"
+            title="Download all"
+            onClick={HandleDownloadAll}
+          >
+            <RippleButton height={36} radius={50} color={"#5454548c"}>
+              <CloudDownloadIcon
+                sx={{ fontSize: 35 }}
+                className="text-neutral-300 cursor-pointer"
+              />
+            </RippleButton>
+          </div>
+        </section>
+
         <SongsList
           songs={user_single_playlist.songs}
           current={"Userplaylist"}

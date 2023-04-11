72713378270cfd2f001abcdae58c3d6867b1c1f7
Dipu <dipuchaurasiya91@gmail.com>
2023-04-11 22:10:53 +0545

    css fixed for playlist

--- src/components/SinglePlaylistCard.jsx
+++ src/components/SinglePlaylistCard.jsx
@@ -8,14 +8,14 @@ const SinglePlaylistCard = ({ name, image, songsLength = 0, playlistId }) => {
       <div className="flex overflow-hidden cursor-pointer hover:bg-lightBlue rounded-md items-center pr-3">
         <div className="flex  w-full items-center gap-5 ">
           {image ? (
-            <img className="rounded-md w-14" src={image} alt="spotify" />
+            <img className="rounded-md w-10" src={image} alt="spotify" />
           ) : (
             <div className="grid place-items-center bg-[#343432] rounded-md p-2">
               <MusicNoteIcon className="text-neutral-300" />
             </div>
           )}
           <div className="">
-            <h3 className="text-neutral-200 tracking-wide text-lg">{name}</h3>
+            <h3 className="text-neutral-200 tracking-wide text-base">{name}</h3>
             <p className="text-neutral-400 text-xs mt-1">{songsLength} songs</p>
           </div>
         </div>

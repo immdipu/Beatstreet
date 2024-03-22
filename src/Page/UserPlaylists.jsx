7a615e2615c95c6e4e0f1e2f8b29546484133c29
Dipu <dipuchaurasiya91@gmail.com>
2024-03-22 12:53:12 +0545

    Refactor UserPlaylists component to remove unused useEffect import and update useState import

--- src/Page/UserPlaylists.jsx
+++ src/Page/UserPlaylists.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 import {

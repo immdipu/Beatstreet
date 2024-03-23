5ba6d42bebe554da12d48c551c26ca164aa706e4
Dipu <dipuchaurasiya91@gmail.com>
2024-03-23 13:07:53 +0545

    recommendation added

--- src/Page/SignUp.jsx
+++ src/Page/SignUp.jsx
@@ -28,7 +28,6 @@ const SignUp = () => {
       }
     },
     onError: (error) => {
-      console.log("error signup", error?.response);
       toast.error(error.response.data.message);
     },
   });

df773639bfe2f70d6f6cc7219dd8e491ec3909ed
Dipu <dipuchaurasiya91@gmail.com>
2024-04-03 12:43:15 +0545

    Refactor SinglePlayLists component to use musicApi.SinglePlaylist instead of userApis.getRecentSongs and fix isLoading variable name in useQuery

--- src/Page/SinglePlayLists.jsx
+++ src/Page/SinglePlayLists.jsx
@@ -17,7 +17,7 @@ const SinglePlayLists = () => {
     queryFn: () => musicApi.SinglePlaylist(id),
   });
 
-  if (loading) {
+  if (isLoading) {
     return (
       <div className="text-2xl font-bold fixed inset-0 w-full h-full flex place-items-center justify-center bg-darkBlue -z-20 max-md:pr-0 pr-32 ">
         <LoadingSpinner size={80} />
